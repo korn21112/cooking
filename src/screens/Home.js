b96ad594b9aee3c3675f472fdcbd648f72d8f446
attakorn <attakorn21112@gmail.com>
2021-07-29 16:57:23 +0700

    fix home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React , {useCallback, useEffect, useRef, useState} from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Alert,
   Pressable,
@@ -16,87 +16,82 @@ import {
   Image,
 } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 
 const Stack = createStackNavigator();
 
-function Home(){
-  
-  const getCurrentUser = () => {
-    const user = auth().currentUser;
-    console.log(user);
-  }
-
+function Home() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
   const navigation = useNavigation();
-  const [name, setName] = useState('');
-  // const [age, setAge] = useState('')
-  const getUser = async () => {
-    const userDocument = await firestore().collection("users").
-    doc('2TPLmvIzSFk6eaPuv6PJ').get()
-    console.log(userDocument);
-    setName(userDocument.data().name)
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
   }
 
   useEffect(() => {
-    getUser();
-    getCurrentUser();
-  },[]);
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    setTimeout(() => {
+      if(!user){
+        navigation.replace('BottomTabNavigator');//('Home');
+      } else {
+        navigation.replace('Login');
+      }
+      
+    },2000);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-  return(
+
+  return (
     <View style={styles.sectionContainer}>
       <Text style={styles.textHeader}>
-        Welcome {name} {auth()?.currentUser.email} to ...!
-      </Text>      
+        Welcome !!!
+      </Text>
       <Image
         style={styles.logo}
         source={require('../../assets/cooking.png')}
       />
-      <TouchableOpacity
-        onPress={()=>{navigation.replace('BottomTabNavigator')}}
-        // onPress={()=>{navigation.navigate('PutIngredient')}}
-      >
-          <Text style={styles.textStart}>Press to Start</Text>
-      </TouchableOpacity>
-
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-      flex: 1,
-    //   justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#FFF3E6',
-    },
-    textHeader: {
-        margin: 10,
-        fontSize: 40,
-        textAlign: 'center',
-        fontFamily:'DancingScript-Regular'
-    },
-    textStart: {
-        fontSize: 20,
-        color: '#FF8C10',
-    },
-    input: {
-      width: 300,
-      borderWidth: 1,
-      borderColor: '#555',
-      borderRadius: 10,
-      backgroundColor: '#ffffff',
-      textAlign: 'center',
-      fontSize: 20,
-      marginTop: 130,
-      marginBottom: 10,
+  sectionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF3E6',
+  },
+  textHeader: {
+    margin: 10,
+    fontSize: 40,
+    textAlign: 'center',
+    fontFamily: 'DancingScript-Regular'
+  },
+  textStart: {
+    fontSize: 20,
+    color: '#FF8C10',
+  },
+  input: {
+    width: 300,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 130,
+    marginBottom: 10,
   },
   logo: {
-    width:200,
-    height:200,
-    margin:20,
-},
-  });
-  
-  export default Home;
+    width: 200,
+    height: 200,
+    margin: 20,
+  },
+});
+
+export default Home;
